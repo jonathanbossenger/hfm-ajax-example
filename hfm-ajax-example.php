df60ff9a1f30afebb5ebf4eac997575b86334821
Jonathan Bossenger <jonathanbossenger@gmail.com>
2022-02-18 16:22:44 +0200

    Moving testimonial block to the main plugin

--- hfm-ajax-example.php
+++ hfm-ajax-example.php
@@ -79,3 +79,16 @@ function hfm_answer_form_submission() {
 		wp_send_json(array('message' => 'Whoops! Please try again.'));
 	}
 }
+
+/**
+ * Registers the block using the metadata loaded from the `block.json` file.
+ * Behind the scenes, it registers also all assets so they can be enqueued
+ * through the block editor in the corresponding context.
+ *
+ * @see https://developer.wordpress.org/reference/functions/register_block_type/
+ */
+function create_block_testimonial_block_block_init() {
+	register_block_type( __DIR__ . '/build' );
+}
+add_action( 'init', 'create_block_testimonial_block_block_init' );
+
--- testimonial-block/build/index.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-element', 'wp-i18n'), 'version' => 'ff24872502aebb8bb008aaedfe5faa67');
